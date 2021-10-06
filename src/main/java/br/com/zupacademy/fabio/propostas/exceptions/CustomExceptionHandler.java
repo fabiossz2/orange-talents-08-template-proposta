7514024a18dcee6161342dde68ac7a65fc45141d
Fabio Santos de Souza <fabio.desouza@zup.com.br>
2021-10-06 08:13:02 -0300

    add handle generico - controller advice

--- src/main/java/br/com/zupacademy/fabio/propostas/exceptions/CustomExceptionHandler.java
+++ src/main/java/br/com/zupacademy/fabio/propostas/exceptions/CustomExceptionHandler.java
@@ -1,16 +1,23 @@
 package br.com.zupacademy.fabio.propostas.exceptions;
 
+import br.com.zupacademy.fabio.propostas.novaproposta.NovaPropostaController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +27,20 @@ public class CustomExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public StandardError handleException(Exception exception) {
+        return new StandardError(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.toString(), exception.getMessage());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<StandardError> handleResponseStatusException(ResponseStatusException exception) {
+        return ResponseEntity.status(exception.getStatus())
+                .body(new StandardError(LocalDateTime.now(), exception.getStatus().value(),
+                        exception.getStatus().toString(), exception.getReason()));
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -52,5 +73,19 @@ public class CustomExceptionHandler {
     public StandardError handleIllegalArgumentException(IllegalArgumentException exception) {
         return new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.toString(), exception.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public StandardError handleConstraintViolationException(ConstraintViolationException exception) {
+        return new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.toString(), exception.getMessage());
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public StandardError handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException exception) {
+        return new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.toString(), exception.getClass().toString());
     }
 }
